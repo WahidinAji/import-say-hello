467f725d955884a3c8b68dc26b0018fa3ce4d588
WahidinAji <a17wahidin@gmail.com>
2021-10-15 05:51:34 +0700

    go get from other package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/WahidinAji/import-say-hello
 
 go 1.16
+
+require github.com/WahidinAji/helper-say-hello v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/WahidinAji/helper-say-hello"
+)
 
+func main() {
+	fmt.Println("hello")
+	say_hello := helper.SayHello()
+	fmt.Println(say_hello)
 }
+
+/*
+remember this one, if the package name (name of function or variable) using Capital in the first words, then it can access on other package.
+if the package name using lowercase, then it can't be accessed by other package
+ */
